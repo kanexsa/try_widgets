78eb7198852506a38285ec49aacad53aba013bd9
udana <udana@denizli.bel.tr>
2023-07-13 15:58:46 +0300

    download button uploaded

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:try_widgets/design/tabs/tabs.dart';
+import 'package:try_widgets/effects/download_button/download_button.dart';
 
-void main() => runApp(const Tabs());
+void main() {
+  runApp(
+    const MaterialApp(
+      home: ExampleCupertinoDownloadButton(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
