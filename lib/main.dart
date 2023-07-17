90dbd2cd3e559c27894e4bb8fe4cc7c6b93409e4
udana <udana@denizli.bel.tr>
2023-07-17 16:21:58 +0300

    nested navigation added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:try_widgets/effects/download_button/download_button.dart';
+import 'package:try_widgets/effects/nested_navigation/home_screen.dart';
+import 'package:try_widgets/effects/nested_navigation/setup_flow.dart';
+
+import 'effects/nested_navigation/settings_screen.dart';
+
+const routeHome = '/';
+const routeSettings = '/settings';
+const routePrefixDeviceSetup = '/setup/';
+const routeDeviceSetupStart = '/setup/$routeDeviceSetupStartPage';
+const routeDeviceSetupStartPage = 'find_devices';
+const routeDeviceSetupSelectDevicePage = 'select_device';
+const routeDeviceSetupConnectingPage = 'connecting';
+const routeDeviceSetupFinishedPage = 'finished';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: ExampleCupertinoDownloadButton(),
+    MaterialApp(
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          appBarTheme: const AppBarTheme(color: Colors.blue),
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: Colors.blue)),
+      onGenerateRoute: (settings) {
+        late Widget page;
+        if (settings.name == routeHome) {
+          page = const HomeScreen();
+        } else if (settings.name == routeSettings) {
+          page = const SettingsScreen();
+        } else if (settings.name!.startsWith(routePrefixDeviceSetup)) {
+          final subRoute =
+              settings.name!.substring(routePrefixDeviceSetup.length);
+          page = SetupFlow(setupPageRoute: subRoute);
+        } else {
+          throw Exception('Unknown route : ${settings.name}');
+        }
+        return MaterialPageRoute<dynamic>(
+          builder: (context) {
+            return page;
+          },
+          settings: settings,
+        );
+      },
       debugShowCheckedModeBanner: false,
     ),
   );
